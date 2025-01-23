d93787f81411ce6abd5304d5a3c75e79796d4ff2
victor <prkone@hotmail.fr>
2025-01-23 23:14:14 +0100

    mock variables tests

--- src/test/java/com/victor/lamontagne_api/ApplicationTests.java
+++ src/test/java/com/victor/lamontagne_api/ApplicationTests.java
@@ -2,8 +2,14 @@ package com.victor.lamontagne_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "MONGO_URL=mongodb://test:test@localhost:27017/test",
+        "JWT_SECRET=test-secret-key-1234567890-test-secret-key",
+        "CLOUDINARY_URL=cloudinary://test:test@test"
+})
 class ApplicationTests {
 
     @Test
