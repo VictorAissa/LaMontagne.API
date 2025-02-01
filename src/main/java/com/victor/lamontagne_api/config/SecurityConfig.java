f174d9003076643010e7e7e42dcb6fb4f44c2e6d
victor <prkone@hotmail.fr>
2025-02-01 16:38:59 +0100

    cors conf test

--- src/main/java/com/victor/lamontagne_api/config/SecurityConfig.java
+++ src/main/java/com/victor/lamontagne_api/config/SecurityConfig.java
@@ -6,15 +6,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +20,7 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig {
     private final JWTAuthFilter jwtAuthFilter;
 
+    @Autowired
     public SecurityConfig(JWTAuthFilter jwtAuthFilter) {
         this.jwtAuthFilter = jwtAuthFilter;
     }
@@ -31,10 +28,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())  // Configuration CORS la plus simple
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/api/**")).permitAll()  // Temporairement autorise tout
+                        .requestMatchers("/api/user/login", "/api/user/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
--- src/main/java/com/victor/lamontagne_api/controller/UserController.java
+++ src/main/java/com/victor/lamontagne_api/controller/UserController.java
@@ -14,6 +14,7 @@ public class UserController {
 
     @Autowired
     public UserController(UserService userService) {
+        System.out.println("==== Requête reçue sur /api/user/login ====");
         this.userService = userService;
     }
 
