4db1ce020bdd9320bb71199a4a6657e367504e02
victor <prkone@hotmail.fr>
2025-02-01 14:46:02 +0100

    cors conf test

--- src/main/java/com/victor/lamontagne_api/config/WebConfig.java
+++ src/main/java/com/victor/lamontagne_api/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
                         "http://127.0.0.1:5173"
