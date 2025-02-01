aaa7658d46259b514c89c6928142273698be8dff
victor <prkone@hotmail.fr>
2025-02-01 16:47:11 +0100

    cors conf test

--- src/main/java/com/victor/lamontagne_api/controller/JourneyController.java
+++ src/main/java/com/victor/lamontagne_api/controller/JourneyController.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/journey")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true", maxAge = 3600)
 public class JourneyController {
     private final JourneyService journeyService;
 
